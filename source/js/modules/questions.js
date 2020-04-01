bcd064f8228ac5719ba64018662c1e78df6c17de
Anton Kiyan <kiyananton@gmail.com>
2020-04-01 23:14:58 +0300

    Исправляет ошибки

--- source/js/modules/questions.js
+++ source/js/modules/questions.js
@@ -2,8 +2,15 @@
   var box = document.querySelector('.questions ul');
   var inputs = document.querySelectorAll('.questions input');
   var labels = document.querySelectorAll('.questions label');
+  var title = document.querySelector('.questions__title');
 
   var idx = -1;
+
+  var scroll = function (target) {
+    return function () {
+      target.scrollIntoView({block: `start`, behavior: `smooth`});
+    }
+  }
 
   var clearInputs = function () {
     inputs.forEach(function (input) {
@@ -17,12 +24,20 @@
     var target = evt.target;
 
     if (target.tagName.toLowerCase() === 'label') {
-      var num = +target.getAttribute('for').slice(-1) - 1;
+
+      var num = +target.getAttribute('for').split('-')[1] - 1;
 
       if (num !== idx) {
         clearInputs();
         idx = num;
       }
+    }
+
+    if (target.tagName.toLowerCase() === 'input') {
+
+      var num = +target.getAttribute('id').split('-')[1] - 1;
+
+      setTimeout(scroll(labels[num]), 100);
     }
   }
 
