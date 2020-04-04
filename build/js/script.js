b37c3417a0af2185a7e502cd4f754b59951fc0d9
Anton Kiyan <kiyananton@gmail.com>
2020-04-04 13:47:59 +0300

    Исправляет таблицу специальных возможностей

--- build/js/script.js
+++ build/js/script.js
@@ -142,57 +142,67 @@
 })();
 
 (function () {
-  var wrapper = document.querySelector('.special-programs__wrapper');
+  var slider = initPreviewSwiper('.special-programs__slider-container');
 
-  if (!wrapper) {
-    return;
-  }
-
-  function onTouchStart(e) {
-    let x = 0;
-
-    function onTouchMove(e) {
-      let touchobj = e.changedTouches[0];
-
-      if (x !== 0) {
-        wrapper.scrollLeft += x - touchobj.clientX;
+  function initPreviewSwiper(name) {
+    return new Swiper(name, {
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
       }
-
-      x = touchobj.clientX;
-    }
-
-    function onTouchEnd(e) {
-      document.removeEventListener(`touchmove`, onTouchMove);
-      document.removeEventListener(`touchend`, onTouchEnd)
-    }
-
-    document.addEventListener(`touchmove`, onTouchMove);
-    document.addEventListener(`touchend`, onTouchEnd);
+    });
   }
+  // var wrapper = document.querySelector('.special-programs__wrapper');
 
-  function onMouseDown(e) {
-    let x = 0;
+  // if (!wrapper) {
+  //   return;
+  // }
 
-    function onMouseMove(e) {
-      e.preventDefault();
+  // function onTouchStart(e) {
+  //   let x = 0;
 
-      if (x !== 0) {
-        wrapper.scrollLeft += x - e.clientX;
-      }
+  //   function onTouchMove(e) {
+  //     let touchobj = e.changedTouches[0];
 
-      x = e.clientX;
-    }
+  //     if (x !== 0) {
+  //       wrapper.scrollLeft += x - touchobj.clientX;
+  //     }
 
-    function onMouseUp(e) {
+  //     x = touchobj.clientX;
+  //   }
 
-      document.removeEventListener(`mousemove`, onMouseMove);
-      document.removeEventListener(`mouseup`, onMouseUp);
-    }
+  //   function onTouchEnd(e) {
+  //     document.removeEventListener(`touchmove`, onTouchMove);
+  //     document.removeEventListener(`touchend`, onTouchEnd)
+  //   }
 
-    document.addEventListener(`mousemove`, onMouseMove);
-    document.addEventListener(`mouseup`, onMouseUp);
-  }
+  //   document.addEventListener(`touchmove`, onTouchMove);
+  //   document.addEventListener(`touchend`, onTouchEnd);
+  // }
 
-  wrapper.addEventListener(`touchstart`, onTouchStart);
-  wrapper.addEventListener(`mousedown`, onMouseDown);
+  // function onMouseDown(e) {
+  //   let x = 0;
+
+  //   function onMouseMove(e) {
+  //     e.preventDefault();
+
+  //     if (x !== 0) {
+  //       wrapper.scrollLeft += x - e.clientX;
+  //     }
+
+  //     x = e.clientX;
+  //   }
+
+  //   function onMouseUp(e) {
+
+  //     document.removeEventListener(`mousemove`, onMouseMove);
+  //     document.removeEventListener(`mouseup`, onMouseUp);
+  //   }
+
+  //   document.addEventListener(`mousemove`, onMouseMove);
+  //   document.addEventListener(`mouseup`, onMouseUp);
+  // }
+
+  //wrapper.addEventListener(`touchstart`, onTouchStart);
+  //wrapper.addEventListener(`mousedown`, onMouseDown);
 })();
